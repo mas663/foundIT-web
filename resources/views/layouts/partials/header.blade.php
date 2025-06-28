ea1f3f88821aa48d299445d8f1ee1ab44cbfd939
Adewangga018 <arjuna.dewangga018@gmail.com>
2025-06-28 16:22:56 +0700

    juna v2

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,16 +1,18 @@
-<header class="w-full flex justify-between items-center p-6 bg-gray-900 border-b border-gray-700 sticky top-0 z-10">
-    <div class="flex items-center space-x-4">
+<header class="w-full flex items-center p-6 bg-gray-900 border-b border-gray-700 sticky top-0 z-10">
+    <div class="flex items-center space-x-4 flex-1 min-w-0">
         <svg class="w-8 h-8 text-white lg:hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
 
-        <div class="relative hidden lg:block">
+        <div class="relative hidden lg:block flex-1 min-w-0">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                 <svg class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
             </span>
-            <input type="text" placeholder="Cari barang hilang" class="w-full max-w-lg bg-gray-800 border border-gray-700 rounded-full py-2 pl-10 pr-4 focus:outline-none focus:ring-2 focus:ring-yellow-500">
+            <input type="text" placeholder="Cari barang hilang" class="w-full bg-gray-800 border border-gray-700 rounded-full py-2 pl-10 pr-4 focus:outline-none focus:ring-2 focus:ring-yellow-500">
         </div>
     </div>
 
-    <div class="flex items-center space-x-4">
-        <div class="w-10 h-10 bg-yellow-500 rounded-full"></div>
+    <div class="flex items-center space-x-4 ml-6 flex-shrink-0">
+        <div class="w-10 h-10 bg-yellow-500 rounded-full flex items-center justify-center">
+            <svg class="w-7 h-7 text-gray-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a5 5 0 00-5 5v1a5 5 0 0010 0V7a5 5 0 00-5-5zm-7 15a7 7 0 0114 0v1a1 1 0 01-1 1H4a1 1 0 01-1-1v-1z" clip-rule="evenodd"/></svg>
+        </div>
     </div>
 </header>
