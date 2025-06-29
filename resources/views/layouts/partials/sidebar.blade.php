9e12569ba585998e120c1ec51266944af4f97444
Adewangga018 <arjuna.dewangga018@gmail.com>
2025-06-29 10:44:22 +0700

    logo foundIT Sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,7 +1,9 @@
 <aside class="w-64 bg-gray-900 p-6 border-r border-gray-700 h-screen sticky top-0">
     <div class="flex items-center space-x-2 mb-10">
         <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        <a href="{{ route('dashboard') }}" class="text-2xl font-bold focus:outline-none">FOUNDit</a>
+        <a href="{{ route('dashboard') }}" class="text-2xl font-bold focus:outline-none">
+            <img src="{{ asset('images/logo.png') }}" alt="FOUNDit Logo" class="w-auto h-8">
+        </a>
     </div>
 
     <nav class="space-y-4">
