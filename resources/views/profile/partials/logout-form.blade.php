ac7c0ad25c83fb11a955fca9613e44dd96001181
DFAFF <dafa.alfarisi39@gmail.com>
2025-06-30 11:27:32 +0700

    Change tampilan logout button

--- resources/views/profile/partials/logout-form.blade.php
+++ resources/views/profile/partials/logout-form.blade.php
@@ -11,10 +11,9 @@
 
     <form method="POST" action="{{ route('logout') }}">
         @csrf
-        <button type="submit" 
-                class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
+        <x-danger-button class="ms-3">
             {{ __('Logout') }}
-        </button>
-        
+        </x-danger-button>
+
     </form>
 </section>
