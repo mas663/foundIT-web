7ea28190076fa65943b50f0de185a2febd8ad0c4
ahmadhilmi9 <hilmiahmad930@gmail.com>
2025-06-30 10:10:50 +0700

    repair

--- resources/views/Lapor/index.blade.php
+++ resources/views/Lapor/index.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="h-screen flex items-center justify-center bg-gray-900 text-white overflow-hidden">
         <div class="w-full max-w-3xl bg-gray-800 rounded-2xl shadow-xl p-10 flex flex-col items-center">
-            <h1 class="text-5xl font-extrabold mb-12 tracking-wide">FOUND<span class="font-light">it</span></h1>
+            <h1 class="text-5xl font-extrabold mb-12 tracking-wide text-white-400">LAPOR</h1>
 
             <a href="{{ route('lapor.kehilangan') }}"
                class="block bg-yellow-400 text-gray-900 text-2xl font-bold px-12 py-6 rounded-xl mb-6 hover:bg-yellow-500 transition-all duration-300 ease-in-out shadow-md w-full text-center">
@@ -14,4 +15,6 @@
                 Lapor Penemuan Barang
             </a>
         </div>
+    </div>
 @endsection
+
