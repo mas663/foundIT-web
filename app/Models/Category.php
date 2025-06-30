e0613cedbea254ea86438770ed7f5f2a3e5c9a9a
auliahanifh <namedauliah@gmail.com>
2025-06-30 12:03:12 +0700

    create: relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function items()
+    {
+        return $this->hasMany(Item::class, 'category_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'category_id');
+    }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -16,5 +16,20 @@ class Item extends Model
     'details',
     'image',
     'status',
-];
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
